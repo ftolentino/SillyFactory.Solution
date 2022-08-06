4901f3bb26b15b95efcc53a3d7025f354d9e8949
ftolentino <filmer.tolentino@gmail.com>
2022-08-06 14:07:11 -0700

    ADD splash index page to show Engineers and their machines

---
+++ Factory/Controllers/EngineerMachineController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineerMachineController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineerMachineController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "View All Engineers and Machines";
+      List<EngineerMachine> model = _db.EngineerMachine.ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Create(EngineerMachine engineermachine)
+    {
+      if (_db.EngineerMachine.FirstOrDefault(
+              em => em.MachineId == engineermachine.MachineId && 
+                    em.EngineerId == engineermachine.EngineerId) == null)
+      {
+        _db.EngineerMachine.Add(engineermachine);
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisEngineerMachine = _db.EngineerMachine.FirstOrDefault(e => e.EngineerMachineId == id);
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == thisEngineerMachine.EngineerId);
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == thisEngineerMachine.MachineId);
+      
+      (EngineerMachine engineermachine, Engineer engineer, Machine machine) model = (thisEngineerMachine, thisEngineer, thisMachine);
+      return View(model);
+    }
+  }
+}
+
