b2350982e110b3ac2706fc1625e2535171f6bbed
ftolentino <filmer.tolentino@gmail.com>
2022-08-05 18:53:15 -0700

    Add AddEngineer method

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -68,5 +68,23 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddEngineer(int id)
+    {
+        var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+        ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+        return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult AddEngineer(Machine machine, int EngineerId)
+    {
+        if (EngineerId != 0)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+          _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
   }
 }
