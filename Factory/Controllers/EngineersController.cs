2fc056c5da0a7473033911047e328de4a310a052
ftolentino <filmer.tolentino@gmail.com>
2022-08-07 17:49:19 -0700

    Bootstrap styling, Add Machine to Engineer Details

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
@@ -55,6 +56,24 @@ namespace Factory.Controllers
       _db.Entry(engineer).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult AddMachine(int id)
+    {
+        var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+        ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Description");
+        return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int MachineId)
+    {
+        if (MachineId != 0)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+          _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
